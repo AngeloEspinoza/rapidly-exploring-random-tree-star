058056824918761808e87ecc42e4ddfe88ca2bd2
AngeloEspinoza <angelodavid98@hotmail.com>
2025-02-25 14:15:03 -0600

    Fix new node problem no recognizing obstacles

--- rrt_star.py
+++ rrt_star.py
@@ -78,95 +78,102 @@ def main():
 			if rand_collision_free:
 				x_near = graph_.nearest_neighbor(tree, x_rand)
 				x_new = graph_.new_state(x_rand, x_near, x_goal)
-				graph_.draw_new_node(map_= environment_.map, x_new=x_new)
-				graph_.draw_random_node(map_=environment_.map)
-				# Find nearest neighbors around the nearest node
-				nearest_neighbors = graph_.nearest_neighbors(tree=tree,
-				                                             x_new=x_new,
-				                                             radius=40)
 
-				# Ensure that there exists a node of the tree around the
-				# random sampled node
-				if len(nearest_neighbors) >= 1:	
-					best_neighbor, best_cost, shortest_path = (
-						graph_.find_best_neighbor_and_shortest_path(
-	                        tree=tree,
-	                        parents=parent,
-	                        nearest_neighbors=nearest_neighbors,
-	                        x_rand=x_new
-						)	
-					)
+				new_collision_free = graph_.is_free(point=x_new,
+				                                    obstacles=obstacles)
 
-					if best_neighbor == None:
-						continue
+				if new_collision_free:
+					# Find nearest neighbors around the nearest node
+					nearest_neighbors = graph_.nearest_neighbors(tree=tree,
+					                                             x_new=x_new,
+					                                             radius=40)
 
-					# Draw the random node shortest path to goal
-					graph_.draw_shortest_neighbor_path(path=shortest_path,
-													   map_=environment_.map,
-													   color=graph_.BLACK)
-					tree.append(x_new)
+					# Ensure that there exists a node of the tree around
+					# the random sampled node
+					if len(nearest_neighbors) >= 1:	
+						best_neighbor, best_cost, shortest_path = (
+							graph_.find_best_neighbor_and_shortest_path(
+		                        tree=tree,
+		                        parents=parent,
+		                        nearest_neighbors=nearest_neighbors,
+		                        x_rand=x_new
+							)	
+						)
 
-					# Add the best_neighbor as the parent of x_rand
-					parent.append(tree.index(best_neighbor))
+						if best_neighbor == None:
+							continue
 
-					# Check and potentially update connections for each
-					# neighbor
-					for neighbor in nearest_neighbors:
-						neighbor_index = tree.index(neighbor)
-						current_cost = graph_.compute_cost_to_start(
-							tree=tree,
-							parents=parent,
-							node_index=neighbor_index)
+						# Draw the random node shortest path to goal
+						graph_.draw_shortest_neighbor_path(
+							path=shortest_path,
+							map_=environment_.map,
+							color=graph_.BLACK)
+						tree.append(x_new)
 
-						# Calculate new cost if x_rand is set as the parent
-						new_cost = (best_cost
-						            + graph_.euclidean_distance(
-	                                    p1=x_new,
-	         							p2=neighbor.center))
+						# Add the best_neighbor as the parent of x_rand
+						parent.append(tree.index(best_neighbor))
 
-						# Update parent if the new path is less expensive
-						if new_cost < current_cost:
-							# Draw over the old connection with background
-							# color to "delete" it
-							old_parent_index = parent[neighbor_index]
-							old_parent_node = tree[old_parent_index]
-							graph_.draw_local_planner(
-								p1=old_parent_node.center,
-								p2=neighbor.center,
-								map_=environment_.map,
-								color=environment_.WHITE)
-
-							# Update parent
-							parent[neighbor_index] = tree.index(x_new)
-							
-							# Redraw the entire tree to avoid erasing other
-							# edges
-							graph_.draw_tree(
+						# Check and potentially update connections for 
+						# each neighbor
+						for neighbor in nearest_neighbors:
+							neighbor_index = tree.index(neighbor)
+							current_cost = graph_.compute_cost_to_start(
 								tree=tree,
-								parent=parent,
-								environment=environment_,
-								should_draw_obstacles=args.obstacles)
+								parents=parent,
+								node_index=neighbor_index)
 
-							# Draw the new connection in red
-							graph_.draw_local_planner(p1=x_new.center,
-							                          p2=neighbor.center,
-							                          map_=environment_.map,
-							                          color=graph_.BLACK)
+							# Calculate new cost if x_rand is set as 
+							# the parent
+							new_cost = (best_cost
+							            + graph_.euclidean_distance(
+		                                    p1=x_new,
+		         							p2=neighbor.center))
+
+							# Update parent if the new path is less
+							# expensive
+							if new_cost < current_cost:
+								# Draw over the old connection with 
+								# background color to "delete" it
+								old_parent_index = parent[neighbor_index]
+								old_parent_node = tree[old_parent_index]
+								graph_.draw_local_planner(
+									p1=old_parent_node.center,
+									p2=neighbor.center,
+									map_=environment_.map,
+									color=environment_.WHITE)
+
+								# Update parent
+								parent[neighbor_index] = tree.index(x_new)
+								
+								# Redraw the entire tree to avoid 
+								#erasing other edges
+								graph_.draw_tree(
+									tree=tree,
+									parent=parent,
+									environment=environment_,
+									should_draw_obstacles=args.obstacles)
+
+								# Draw the new connection in red
+								graph_.draw_local_planner(
+									p1=x_new.center,
+									p2=neighbor.center,
+									map_=environment_.map,
+									color=graph_.BLACK)
 
 
-							# Draw the full path from start to the neighbor
-							# through the new connection
-							path = graph_.find_shortest_path_to_start(
-								tree=tree,
-	                        	parents=parent,
-	                        	start_index=tree.index(initial),
-	                        	node_index=neighbor_index)
-							graph_.draw_shortest_neighbor_path(
-								path=path,
-								map_=environment_.map,
-								color=graph_.BLACK)
+								# Draw the full path from start to the
+								# neighbor through the new connection
+								path = graph_.find_shortest_path_to_start(
+									tree=tree,
+		                        	parents=parent,
+		                        	start_index=tree.index(initial),
+		                        	node_index=neighbor_index)
+								graph_.draw_shortest_neighbor_path(
+									path=path,
+									map_=environment_.map,
+									color=graph_.BLACK)
 
-					graph_.number_of_nodes = len(tree)
+						graph_.number_of_nodes = len(tree)
 									
 			pygame.display.update()
 	pygame.quit()
